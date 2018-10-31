16b86ea77ed603f4041a0707533f13dde82e90f4
ssreekanth2000 <s.sreekanth2000@gmail.com>
2018-10-31 19:41:35 -0400

    Update sum.asm

--- asm/sum.asm
+++ asm/sum.asm
@@ -3,14 +3,14 @@
 #Using the sum of n =n(n+1)/2
 
 
-add $t0 ,$t0,15# N being declared
-add $t1 ,$t0 ,1# N+1 being created
-add $t3 , $t3, 0 # Counter for repeated addition
-add $t4, $t4,0 #Counter for repeated substraction
-add $t5 ,$t5,0# Just a zero value
+li $t0 ,15# N being declared
+li $t1 ,1# N+1 being created
+li $t3 , 0 # Counter for repeated addition
+li $t4, 0 #Counter for repeated substraction
+li $t5 ,0# Just a zero value
 label1:
 add $t2, $t0, $t2 # where N(N+1) is stored
-add $t3, $t3, 1
+addi $t3, $t3, 1
 bne $t1, $t3 label1 #BNE to check completeness of N(N+1) using the counter
 label2:
 add  $t2, $t2, -2#Repeated subtraction of 2 to divivide N(N+1) by 2
