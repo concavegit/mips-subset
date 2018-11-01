dade257423e661ae7d9f4cf20c6f854839b5f941
ssreekanth2000 <s.sreekanth2000@gmail.com>
2018-11-01 19:47:42 -0400

    Update fib.asm

--- asm/fib.asm
+++ asm/fib.asm
@@ -1,15 +1,15 @@
-li $t0,1
-li $t1,1
-li $t2,6
-li $t4,0
-li $t3,0
+addi $t0,$t0,1
+addi $t1,$t1,1
+addi $t2,$t2,6
+addi $t4,$t4,0
+addi $t3,$t3,0
 label1:
 
-add $t4, $t1,0
+add1 $t4, $t1,0
 add $t1, $t1, $t0
-add $t0, $t4,0
-add $t3,$t3, 1
+addi $t0, $t4,0
+addi $t3,$t3, 1
 bne $t3 $t2 label1
 
 
-add $v1, $t1,0
+add $v0, $t1,0
