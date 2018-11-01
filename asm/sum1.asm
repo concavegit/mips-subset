68e4ab4c4cd5c28f096ec9821521363112d745bd
ssreekanth2000 <s.sreekanth2000@gmail.com>
2018-11-01 13:02:54 -0400

    Update sum1.asm

--- asm/sum1.asm
+++ asm/sum1.asm
@@ -2,11 +2,11 @@
 #Using the sum of n =n(n+1)/2
 
 
-addi $t0 ,$t0,15# N being declared
+addi $t0 ,$zero,15# N being declared
 addi $t1 ,$t0,1# N+1 being created
-addi $t3 ,$t3, 0 # Counter for repeated addition
-addi $t4, $t4, 0 #Counter for repeated substraction
-addi $t5 ,$t5,0# Just a zero value
+addi $t3 ,$zero, 0 # Counter for repeated addition
+addi $t4, $zero, 0 #Counter for repeated substraction
+addi $t5 ,$zero,0# Just a zero value
 label1:
 add $t2, $t0, $t2 # where N(N+1) is stored
 addi $t3, $t3, 1
